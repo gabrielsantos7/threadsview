b41df5ce7b3e7570e0ca47e841418aa585322107
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-11-07 06:10:00 -0300

    add requirements

--- app.py
+++ app.py
@@ -6,89 +6,98 @@ from pathlib import Path
 
 @st.cache_data
 def load_data():
-    path = Path(__file__).parent / "dataset" / "threads.xlsx"
+    # Define o caminho do arquivo
+    file_path = Path(__file__).parent / "dataset" / "threads.xlsx"
 
     # Carrega todas as planilhas em um dicionário de DataFrames
-    data_dict = pd.read_excel(path, sheet_name=None)
+    df = pd.read_excel(file_path, sheet_name="Filtered_Posts")
 
     # Concatena todos os DataFrames em um único DataFrame
-    combined_df = pd.concat(data_dict.values(), ignore_index=True)
+    # combined_df = pd.concat(sheet_data.values(), ignore_index=True)
 
-    # Converter a coluna "Data e Hora" para datetime
-    combined_df["Data e Hora"] = pd.to_datetime(
-        combined_df["Data e Hora"], errors='coerce', format="%d/%m/%Y %H:%M:%S"
+    # Converte a coluna "Data e Hora" para o tipo datetime
+    df["Data e Hora"] = pd.to_datetime(
+        df["Data e Hora"], errors='coerce', format="%d/%m/%Y %H:%M:%S"
     )
-    print(combined_df)
-    return combined_df
+
+    return df
 
 
 def main():
-    # Configura o layout para ocupar toda a largura da tela
+    # Configura o layout da página
     st.set_page_config(layout='wide')
 
-    # Lê o conteúdo do arquivo README.md
+    # Carrega o conteúdo do arquivo README.md
     with open("README.md", "r", encoding="utf-8") as file:
         readme_content = file.read()
-
-    # Renderiza o conteúdo em Markdown
     st.markdown(readme_content)
 
-    # Carregando e tratando os dados
-    df = load_data()
+    # Carrega os dados
+    data = load_data()
 
+    # Configura filtros na barra lateral
     st.sidebar.title("Filtros")
-    keywords = st.sidebar.multiselect(
-        "Palavras - Chave",
-        df["Palavras-Chave"].unique(),
-        df["Palavras-Chave"].unique()
+    selected_keywords = st.sidebar.multiselect(
+        "Palavras-Chave",
+        data["Palavras-Chave"].unique(),
+        default=data["Palavras-Chave"].unique()
     )
 
-    # Definindo o intervalo de datas usando a coluna convertida
-    date_interval = st.sidebar.date_input(
+    # Define o intervalo de datas com base nos dados
+    date_range = st.sidebar.date_input(
         "Intervalo de datas",
-        (df["Data e Hora"].min().date(), df["Data e Hora"].max().date()),
-        min_value=df["Data e Hora"].min().date(),
-        max_value=df["Data e Hora"].max().date(),
-        format="DD/MM/YYYY"
+        (data["Data e Hora"].min().date(), data["Data e Hora"].max().date()),
+        min_value=data["Data e Hora"].min().date(),
+        max_value=data["Data e Hora"].max().date()
     )
+    start_date, end_date = pd.to_datetime(date_range[0]), pd.to_datetime(date_range[1])
 
-    # Convertendo o intervalo de data para datetime64
-    start_date = pd.to_datetime(date_interval[0])
-    end_date = pd.to_datetime(date_interval[1])
-
-    # Aplicando os filtros
-    df_filtered = df[
-        (df["Palavras-Chave"].isin(keywords)) &
-        (df["Data e Hora"].between(start_date, end_date))
+    # Filtra os dados com base nos filtros selecionados
+    filtered_data = data[
+        (data["Palavras-Chave"].isin(selected_keywords)) &
+        (data["Data e Hora"].between(start_date, end_date))
     ]
 
-    if keywords:
-        st.subheader("Threads coletadas")
-        st.write(f'Foram coletadas um total de {
-            len(df_filtered)} threads com as palavras-chave: {", ".join(keywords)} no intervalo de tempo selecionado.')
-        st.dataframe(df_filtered)
+    if selected_keywords:
+        # Seção de análises gerais
+        st.subheader("Visão Geral sobre o conteúdo coletado")
+        col1, col2, col3, col4 = st.columns(4)
+        
+        # Exibe métricas formatadas
+        col1.metric("Número de threads", f"{len(filtered_data):,}".replace(",", "."))
+        col2.metric("Soma de curtidas", f"{filtered_data['Curtidas'].sum():,}".replace(",", "."))
+        col3.metric("Soma de comentários", f"{filtered_data['Comentários'].sum():,}".replace(",", "."))
+        col4.metric("Soma de republicações", f"{filtered_data['Republicações'].sum():,}".replace(",", "."))
 
+        # Exibe o DataFrame filtrado
+        st.dataframe(filtered_data)
+
+        # Preparação dos dados para o gráfico de engajamento
         st.subheader("Análises gráficas")
-        comments_fig = px.bar(df_filtered, x="Palavras-Chave", y="Comentários",
-                              title="Números de comentários por palavra-chave")
-        st.plotly_chart(comments_fig)
+        engagement_summary = filtered_data.groupby("Palavras-Chave")[["Curtidas", "Comentários", "Republicações"]].sum().reset_index()
+        engagement_summary["Engajamento Total"] = engagement_summary[["Curtidas", "Comentários", "Republicações"]].sum(axis=1)
 
-        posts_por_data = df_filtered.groupby(df_filtered['Data e Hora'].dt.date).size(
-        ).reset_index(name='Número de posts')
-        date_fig = px.line(posts_por_data, x="Data e Hora", y="Número de posts",
-                           title="Número de posts ao longo do período")
-        st.plotly_chart(date_fig)
+        # Gráfico de barras para engajamento total
+        engagement_chart = px.bar(
+            engagement_summary, x="Palavras-Chave", y="Engajamento Total",
+            title="Engajamento (Soma de Curtidas, Comentários e Republicações) por Palavra-Chave"
+        )
+        st.plotly_chart(engagement_chart)
+
+        # Preparação e visualização do gráfico de posts por data
+        posts_by_date = filtered_data.groupby(filtered_data['Data e Hora'].dt.date).size().reset_index(name='Número de posts')
+        date_chart = px.line(posts_by_date, x="Data e Hora", y="Número de posts", title="Número de posts ao decorrer do tempo")
+        st.plotly_chart(date_chart)
 
     else:
-        st.warning(
-            "Selecione ao menos uma palavra-chave para apresentar os resultados.")
+        st.warning("Selecione ao menos uma palavra-chave para apresentar os resultados.")
 
+    # Seção de conclusão
     st.subheader("Conclusão")
     st.write("""
     A pesquisa revelou que as threads coletadas abordaram uma ampla gama de assuntos relacionados ao Brasil e à cultura afro-brasileira, destacando a riqueza e diversidade dos tópicos discutidos. A ferramenta desenvolvida demonstrou ser altamente eficaz na coleta e análise desses dados, provando seu potencial para ser reutilizada em pesquisas envolvendo diferentes temas e palavras-chave.
 
     Apesar do sucesso obtido, a rede social estudada apresenta algumas limitações em termos de busca e filtragem de conteúdo, especialmente quando comparada ao Twitter. A plataforma do Twitter permite a realização de pesquisas complexas com múltiplos filtros, como data de publicação, idioma, geolocalização e outros parâmetros avançados. Essas funcionalidades ampliam as possibilidades de análise e podem representar uma vantagem em projetos de coleta de dados mais detalhados e personalizados.
     """)
-
 
 main()
